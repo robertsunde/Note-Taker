1776f5cdc1ed8afe4208606391f730836a028028
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-02-18 22:53:03 -0500

    html routes file contents written

--- Develop/public/Routes/htmlroutes.js
+++ Develop/public/Routes/htmlroutes.js
@@ -1,1 +1,33 @@
 // HTML Routes
+
+const path = require(`path`);
+
+module.exports = (app) => {
+
+
+app.get(`/notes`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../notes.html`));
+});
+
+
+app.get(`/index`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../index.html`));
+});
+
+
+app.get(`/*`, (req, res) => {
+    res.sendFile(path.join(__dirname, `../index.html`));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+};
