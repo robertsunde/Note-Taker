d5780d5c62a5997417b9b8f0ad01df94aba7bd65
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-02-20 18:58:32 -0500

    routing apiroutes file to read/write notes properly

--- Routes/apiroutes.js
+++ Routes/apiroutes.js
@@ -6,11 +6,22 @@ const dbfile = require ("../db/db.json")
 // module.exports = (app) => {
 
 app.get("/notes", (req, res) => {
-dbfile = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+let dbfile = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
 res.json(dbfile)
 
 }) 
 
+app.post("/notes", (req, res) => {
+let noteContent = {
+title: req.body.title,
+text: req.body.text,
+id: Math.floor(Math.random()*100000)
+}
+
+dbfile.push(noteContent)
+fs.writeFileSync("../db/db.json", JSON.stringify(dbfile))
+res.JSON(dbfile)
+})
 //app.post here createnote object with title, text, id generated randomly. push this into db.json file using writefilesync
 
 
